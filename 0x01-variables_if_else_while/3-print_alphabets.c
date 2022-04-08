ec61a158a3fb508e62af75861f0553bb91213785
Eloagbawe <eloagbawe@gmail.com>
2022-04-08 14:33:40 -0700

    add script

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *  *  *  *  * main - Entry point
+ *   *   *   *   *
+ *    *    *    *    * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char upper;
+char lower;
+
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+	putchar(lower);
+}
+for (upper = 'A'; upper <= 'Z'; upper++)
+{
+	putchar(upper);
+}
+putchar('\n');
+return (0);
+}
