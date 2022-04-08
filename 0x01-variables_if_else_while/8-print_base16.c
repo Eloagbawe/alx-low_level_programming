fcbe8bfd6ef45e36f9a83ea46e58e2aa41e46771
Eloagbawe <eloagbawe@gmail.com>
2022-04-08 16:28:57 -0700

    fix script

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ for (x = 0; x < 10; x++)
 {
 	putchar((x % 10) + '0');
 }
-for (alphabet = 'A'; alphabet <= 'F'; alphabet++)
+for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
 {
 	putchar(alphabet);
 }
