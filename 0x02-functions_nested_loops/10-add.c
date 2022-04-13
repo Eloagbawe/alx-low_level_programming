6bef7f80980a2e6b71f1fb6206a66a5b49148300
Eloagbawe <eloagbawe@gmail.com>
2022-04-12 20:16:00 -0700

    add sum function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers together
+ * @x: number to be checked
+ * @y: number to be checked
+ * Return: addition of both numbers
+*/
+int add(int x, int y)
+{
+	return (x + y);
+}
