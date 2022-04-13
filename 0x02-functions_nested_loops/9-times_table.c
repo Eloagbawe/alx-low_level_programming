292c2f93bfdc116b3c62da9dd0432a212b286c12
Eloagbawe <eloagbawe@gmail.com>
2022-04-12 20:10:25 -0700

    update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,13 @@ for (i = 0; i < 10; i++)
 	for (j = 0; j < 10; j++)
 	{
 		k = i * j;
-		if (k < 10)
+		if ((k < 10) && (j != 0))
 		{
 			_putchar(' ');
+			_putchar((k % 10) + '0');
+		}
+		if (j == 0)
+		{
 			_putchar((k % 10) + '0');
 		}
 		if (k >= 10)
