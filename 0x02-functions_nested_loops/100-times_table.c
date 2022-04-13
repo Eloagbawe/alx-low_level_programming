d33a6c56ac2336fcc1490e480c68391a4ba55c1d
Eloagbawe <eloagbawe@gmail.com>
2022-04-13 08:31:13 -0700

    update 100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,22 +8,22 @@ void print_times_table(int n)
 {
 int i, j, k;
 
-if ((n < 15) && (n > 0))
+if ((n < 15) && (n > -1))
 {
 for (i = 0; i <= n; i++)
 {
 	for (j = 0; j <= n; j++)
 	{
 	k = i * j;
+	if (j == 0)
+	{
+		_putchar(k + '0');
+	}
 	if ((k < 10) && (j != 0))
 	{
 		_putchar(' ');
 		_putchar(' ');
-		_putchar((k % 10) + '0');
-	}
-	if (j == 0)
-	{
-		_putchar((k % 10) + '0');
+		_putchar(k + '0');
 	}
 	if ((k >= 10) && (k < 100))
 	{
