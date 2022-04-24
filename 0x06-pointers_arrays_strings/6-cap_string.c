3013e93c4dad66f27b2e609a84505b0035c397c5
Eloagbawe <eloagbawe@gmail.com>
2022-04-24 12:05:10 -0700

    add function that capitalizes each word in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes every word in a string
+ * @s: string to be modified
+ * Return: modified string
+*/
+char *cap_string(char *s)
+{
+	int a;
+	char alph;
+
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		if ((s[a] == 32) || (s[a] == 9) || (s[a] == 10)
+			|| (s[a] == 44) || (s[a] == 59) ||
+			(s[a] == 59) || (s[a] == 46) ||
+			(s[a] == 33) || (s[a] == 63) ||
+			(s[a] == 34) || (s[a] == 40) ||
+			(s[a] == 41) || (s[a] == 123) ||
+			(s[a] == 125))
+		{
+			alph = 'a';
+
+			while (alph <= 'z')
+			{
+				if (s[a + 1] == alph)
+				{
+					s[a + 1] = s[a + 1] - 32;
+				}
+				alph++;
+			}
+		}
+		a++;
+	}
+	return (s);
+}
