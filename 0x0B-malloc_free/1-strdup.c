18fe6af698ae2a6c30f160975f3f86684943be59
Eloagbawe <eloagbawe@gmail.com>
2022-05-12 14:39:45 -0700

    update main.h

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - returns a pointer to a duplicate string
+ * @str: string to be duplicated
+ * Return: pointer
+*/
+char *_strdup(char *str)
+{
+
+}
