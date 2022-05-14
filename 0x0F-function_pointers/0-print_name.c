8a571e7cc62846028a24e6d013ea818e021f65e7
Eloagbawe <eloagbawe@gmail.com>
2022-05-13 19:08:48 -0700

    update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
  * print_name - prints a name
  * @name: string
@@ -9,10 +9,9 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
+	if (name && f)
 	{
-		return;
+		(*f)(name);
 	}
-	(*f)(name);
 }
 
