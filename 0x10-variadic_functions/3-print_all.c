34fb1a19924461e5d0292d253d075a4eb25f8908
Eloagbawe <eloagbawe@gmail.com>
2022-05-17 07:21:02 -0700

    update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,58 @@
 #include <stdio.h>
 #include "variadic_functions.h"
+
+/**
+ * print_int - prints an integer
+ * @ap: arguments
+ * Return: void
+*/
+
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+ * print_char - prints a character
+ * @ap: arguments
+ * Return: void
+*/
+
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+ * print_float - prints a float
+ * @ap: arguments
+ * Return: void
+*/
+
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_string - prints a string
+ * @ap: arguments
+ * Return: void
+*/
+
+void print_string(va_list ap)
+{
+	const char *p = va_arg(ap, char *);
+
+	if (p == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s", p);
+	}
+}
 /**
  * print_all - function that prints anything
  * @format: variable format
@@ -8,39 +61,32 @@
 
 void print_all(const char * const format, ...)
 {
-	int i;
+	int i, j;
 	va_list ap;
 
-	char a = 'c', b = 'i', c = 'f', d = 's';
-
+	pt_t pts[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
 	i = 0;
 	va_start(ap, format);
 	while (format[i] != '\0')
 	{
-		if (format[i] == a)
-		{
-			printf("%c, ", va_arg(ap, int));
-		}
-		else if (format[i] == d)
-		{
-			const char * const p = va_arg(ap, char*);
+		j = 0;
 
-			if (p == NULL)
-			{
-				printf("(nil)");
-			}
-			else
-			{
-				printf("%s, ", p);
-			}
-		}
-		else if (format[i] == b)
+		while (j < 4)
 		{
-			printf("%d, ", va_arg(ap, int));
-		}
-		else if (format[i] == c)
-		{
-			printf("%f, ", va_arg(ap, double));
+			if (format[i] == *pts[j].c)
+			{
+				pts[j].f(ap);
+
+				if (format[i + 1] != '\0')
+				{
+					printf(", ");
+				}
+			}	j++;
 		}
 		i++;
 	}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,4 +8,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct pt - prints type
+ * @c: the character
+ * @f: function
+*/
+
+typedef struct pt
+{
+	char *c;
+	void (*f)();
+} pt_t;
+
 #endif
