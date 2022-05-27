eb58c7678b763fc33c502f55248b8fb3e1f1205a
Eloagbawe <eloagbawe@gmail.com>
2022-05-26 17:57:33 -0700

    update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,17 +12,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current_node;
 
-	if (*head == NULL)
+	if (head != NULL)
 	{
-		return;
+		while (*head != NULL)
+		{
+			current_node = *head;
+			*head = (current_node)->next;
+			free(current_node);
+		}
 	}
-	current_node = *head;
-
-	while (*head != NULL)
-	{
-		current_node = *head;
-		*head = (*head)->next;
-		free(current_node);
-	}
-	*head = NULL;
 }
