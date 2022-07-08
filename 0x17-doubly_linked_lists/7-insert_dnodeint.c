052c5344ae59d1c00b877bf356f8f15bfa63e0b9
Eloagbawe <eloagbawe@gmail.com>
2022-07-07 18:40:26 -0700

    update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,39 +12,32 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *new_node, *temp;
+	dlistint_t *new_node, *temp = *h;
 
-	if (*h == NULL)
+	if (h == NULL || (temp == NULL && idx > 0))
 		return (NULL);
-	if (i == idx)
+	if (idx == 0)
 	{
 		new_node = add_dnodeint(h, n);
 		return (new_node);
 	}
-	temp = *h;
 	while (temp != NULL)
 	{
 		if (i == idx)
 		{
-			if (temp->next == NULL)
-				new_node = add_dnodeint_end(h, n);
-			else
-			{
-				new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-				if (new_node == NULL)
-					return (NULL);
-				new_node->n = n;
-				new_node->next = temp;
-				new_node->prev = temp->prev;
-				if (temp->prev != NULL)
-					temp->prev->next = new_node;
-			}
+			new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+			if (new_node == NULL)
+				return (NULL);
+			new_node->n = n;
+			new_node->next = temp;
+			new_node->prev = temp->prev;
+			temp->prev->next = new_node;
 			return (new_node);
 		}
 		i += 1;
 		temp = temp->next;
 	}
-	if (i == idx)
+	if (temp == NULL && i == idx)
 	{
 		new_node = add_dnodeint_end(h, n);
 		return (new_node);
