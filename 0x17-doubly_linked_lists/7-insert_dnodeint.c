da6360e134cc265a2667c30bfaf1f5b590faf02d
Eloagbawe <eloagbawe@gmail.com>
2022-07-07 17:21:55 -0700

    update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,10 +30,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (i == idx)
 		{
 			new_node->n = n;
-			new_node->next = temp->prev->next;
-			temp->prev->next = new_node;
-			new_node->prev = temp->next->prev;
-			temp->next->prev = new_node;
+			new_node->next = temp;
+			new_node->prev = temp->prev;
+			if (temp->prev != NULL)
+				temp->prev->next = new_node;
 			return (new_node);
 		}
 		i += 1;
