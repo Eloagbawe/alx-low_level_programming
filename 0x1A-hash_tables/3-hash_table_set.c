c81dadbfc3b7452910901c1d330a0422fc6debcf
Eloagbawe <eloagbawe@gmail.com>
2022-07-29 15:48:57 -0700

    update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,10 +14,18 @@ hash_node_t *ht_pair(const char *key, const char *value)
 
 	if (new_node == NULL)
 		return (NULL);
-	if (strcmp(key, "") == 0)
-		return (NULL);
 	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->next = NULL;
 
 	return (new_node);
@@ -35,8 +43,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *entry;
 
-	if (ht == NULL || value == NULL ||
-		key == NULL || *key == '\n' || *value == '\n')
+	if (ht == NULL || key == NULL || value == NULL ||
+		*key == '\0' || *key == '\n' || *value == '\n')
 	{
 		return (0);
 	}
@@ -56,7 +64,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(entry->key, key) == 0)
 		{
 			free(entry->value);
-			strcpy(entry->value, value);
+			entry->value = strdup(value);
 			return (1);
 		}
 		entry = entry->next;
