82edc15010138e155e5c9edcca63ff07a48d3ffd
Eloagbawe <eloagbawe@gmail.com>
2022-11-04 16:57:17 +0100

    update jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -17,7 +17,7 @@ int jump_search(int *array, size_t size, int value)
 	if (array != NULL)
 	{
 		current = 0;
-		while (current < size)
+		while (current < size - 1)
 		{
 			if (array[current] < value)
 			{
